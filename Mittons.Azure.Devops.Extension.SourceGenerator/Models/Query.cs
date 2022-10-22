ca605c5563e549f317a25e0d8e3ba11b8e2bcb32
ktmitton <kdrive113@gmail.com>
2022-10-22 19:08:53 -0400

    Removed usings

--- Mittons.Azure.Devops.Extension.SourceGenerator/Models/Query.cs
+++ Mittons.Azure.Devops.Extension.SourceGenerator/Models/Query.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Mime;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
