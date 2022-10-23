0593f6f4428d8354bf6a3d2052902ef4a8408b93
ktmitton <kdrive113@gmail.com>
2022-10-23 01:01:55 -0400

    Cleaned up tests for better intellisense

--- Mittons.Azure.Devops.Extension.SourceGenerator/ServiceSourceGenerator.cs
+++ Mittons.Azure.Devops.Extension.SourceGenerator/ServiceSourceGenerator.cs
@@ -13,50 +13,6 @@ namespace Mittons.Azure.Devops.Extension.SourceGenerator
     [Generator]
     public class ServiceSourceGenerator : ISourceGenerator
     {
-        // determine the namespace the class/enum/struct is declared in, if any
-        static string GetNamespace(InterfaceDeclarationSyntax syntax)
-        {
-            // If we don't have a namespace at all we'll return an empty string
-            // This accounts for the "default namespace" case
-            var nameSpace = string.Empty;
-
-            // Get the containing syntax node for the type declaration
-            // (could be a nested type, for example)
-            SyntaxNode potentialNamespaceParent = syntax.Parent;
-
-            // Keep moving "out" of nested classes etc until we get to a namespace
-            // or until we run out of parents
-            while (potentialNamespaceParent != null &&
-                !typeof(NamespaceDeclarationSyntax).IsAssignableFrom(potentialNamespaceParent.GetType()) &&
-                !typeof(FileScopedNamespaceDeclarationSyntax).IsAssignableFrom(potentialNamespaceParent.GetType()))
-            {
-                potentialNamespaceParent = potentialNamespaceParent.Parent;
-            }
-
-            // Build up the final namespace by looping until we no longer have a namespace declaration
-            if (potentialNamespaceParent is BaseNamespaceDeclarationSyntax namespaceParent)
-            {
-                // We have a namespace. Use that as the type
-                nameSpace = namespaceParent.Name.ToString();
-
-                // Keep moving "out" of the namespace declarations until we
-                // run out of nested namespace declarations
-                while (true)
-                {
-                    if (!typeof(NamespaceDeclarationSyntax).IsAssignableFrom(potentialNamespaceParent.Parent.GetType()))
-                    {
-                        break;
-                    }
-
-                    // Add the outer namespace as a prefix to the final namespace
-                    nameSpace = $"{namespaceParent.Name}.{nameSpace}";
-                    namespaceParent = namespaceParent.Parent as NamespaceDeclarationSyntax;
-                }
-            }
-
-            // return the final namespace
-            return nameSpace;
-        }
         public void Execute(GeneratorExecutionContext context)
         {
             // Get our SyntaxReceiver back
@@ -71,7 +27,7 @@ namespace Mittons.Azure.Devops.Extension.SourceGenerator
 
                 var typeSymbol = serviceModel.GetSymbolInfo(ids).Symbol as INamedTypeSymbol;
 
-                var classNamespace = GetNamespace(ids);
+                var classNamespace = ids.GetNamespace();
 
                 var serviceAttribute = ids.AttributeLists
                     .Select(x => x.Attributes)
