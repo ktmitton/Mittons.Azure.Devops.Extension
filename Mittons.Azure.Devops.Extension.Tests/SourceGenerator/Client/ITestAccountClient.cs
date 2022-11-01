c4396989dbc2c615ca72552458365c64ec0f5d4d
ktmitton <kdrive113@gmail.com>
2022-10-31 21:22:38 -0400

    Removed old files

--- Mittons.Azure.Devops.Extension.Tests/SourceGenerator/Client/ITestAccountClient.cs
+++
@@ -1,9 +0,0 @@
-using Mittons.Azure.Devops.Extension.Api.Attributes;
-using Mittons.Azure.Devops.Extension.Client;
-
-namespace Mittons.Azure.Devops.Extension.Tests.SourceGenerator.Client;
-
-[GenerateClient(ResourceAreaId.Accounts)]
-public interface ITestAccountsClient
-{
-}
