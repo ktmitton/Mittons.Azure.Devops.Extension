0941a945a34b8e9adde0b50930c60c8720524be1
ktmitton <kdrive113@gmail.com>
2022-11-24 16:53:24 -0500

    Cleaned up code

--- Mittons.Azure.Devops.Extension.SourceGenerator/Service/ServiceSourceGenerator.cs
+++ Mittons.Azure.Devops.Extension.SourceGenerator/Service/ServiceSourceGenerator.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net.Mime;
 using System.Text;
 using System.Text.RegularExpressions;
 using HandlebarsDotNet;
@@ -9,7 +8,6 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 using Mittons.Azure.Devops.Extension.SourceGenerator.Extensions;
-using Mittons.Azure.Devops.Extension.SourceGenerator.Models;
 using Mittons.Azure.Devops.Extension.SourceGenerator.Utilities;
 
 namespace Mittons.Azure.Devops.Extension.SourceGenerator.Service
@@ -57,29 +55,6 @@ namespace Mittons.Azure.Devops.Extension.SourceGenerator.Service
                     var methodName = method.Identifier.Text;
 
                     return $"[property: JsonPropertyName(\"{jsonPropertyName}\")] RemoteProxyFunctionDefinition {methodName}";
-
-                    // var query = new Query(method);
-
-                    // var requestBody = new RequestBody(serviceModel, method);
-
-                    // var innerReturnType = Regex.Replace(method.ReturnType.ToString(), @"^Task<(.*)>$", "$1");
-                    // var nonNullableInnerReturnType = Regex.Replace(innerReturnType, @"^(.*)\?$", "$1");
-
-                    // return new
-                    // {
-                    //     MethodName = method.Identifier.Text,
-                    //     HttpMethod = serviceModel.GetConstantValue(clientRequestAttribute.ArgumentList.Arguments[1].Expression).ToString(),
-                    //     ReturnType = method.ReturnType.ToString(),
-                    //     InnerReturnType = innerReturnType,
-                    //     NonNullableInnerReturnType = nonNullableInnerReturnType,
-                    //     IsInnerReturnTypeNullable = Regex.IsMatch(innerReturnType, @"\?$"),
-                    //     ParametersList = string.Join(", ", method.ParameterList.Parameters.Select(x => $"{x.Type} {x.Identifier.ValueText}")),
-                    //     RequestApiVersion = serviceModel.GetConstantValue(clientRequestAttribute.ArgumentList.Arguments[0].Expression).ToString(),
-                    //     RequestAcceptType = clientRequestAttribute.ArgumentList.Arguments.Count > 3 ? serviceModel.GetConstantValue(clientRequestAttribute.ArgumentList.Arguments[3].Expression).ToString() : "application/json",
-                    //     RouteTemplate = serviceModel.GetConstantValue(clientRequestAttribute.ArgumentList.Arguments[2].Expression).ToString(),
-                    //     QueryParametersList = query.Parameters,
-                    //     RequestBody = requestBody
-                    // };
                 });
 
                 var remoteProxyFunctionImplementations = methods.Select(method =>
@@ -118,13 +93,6 @@ namespace Mittons.Azure.Devops.Extension.SourceGenerator.Service
                     RemoteProxyFunctionDefinitions = string.Join(", ", remoteProxyFunctionDefinitions),
                     RemoteProxyFunctionVoidImplementations = remoteProxyFunctionImplementations.Where(x => x.InnerReturnType.Length == 0),
                     RemoteProxyFunctionGenericImplementations = remoteProxyFunctionImplementations.Where(x => x.InnerReturnType.Length > 0)
-                    // ResourceAreaId = resourceAreaId,
-                    // ByteArrayMethods = convertedMethods.Where(x => x.InnerReturnType == "byte[]"),
-                    // JsonMethods = convertedMethods.Where(x => x.RequestAcceptType == "application/json"),
-                    // StringMethods = convertedMethods.Where(x => x.RequestAcceptType != "application/json" && x.RequestAcceptType != MediaTypeNames.Application.Zip && Regex.IsMatch(x.InnerReturnType, @"^string\??$")),
-                    // XmlDocumentMethods = convertedMethods.Where(x => x.InnerReturnType == "XmlDocument"),
-                    // XmlMethods = convertedMethods.Where(x => x.RequestAcceptType == "application/xml" && x.InnerReturnType != "string" && x.InnerReturnType != "byte[]" && x.InnerReturnType != "XmlDocument"),
-                    // ZipArchiveMethods = convertedMethods.Where(x => x.RequestAcceptType == MediaTypeNames.Application.Zip && x.InnerReturnType == "ZipArchive")
                 };
 
                 context.AddSource($"{className}.g.cs", SourceText.From(compiled(data), Encoding.UTF8));
